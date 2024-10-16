a174f55ffc65250196fe51a44eef40455d824580
Andres Gomez Tagle Azcarraga <gtaglea@hotmail.com>
2024-10-16 15:07:24 -0600

    boton generico

--- legalweb/src/components/boton.jsx
+++ legalweb/src/components/boton.jsx
@@ -1,1 +1,16 @@
-//boton
+import * as React from "react";
+import Button from "@mui/material/Button";
+
+export default function CustomButton({
+  text,
+  onClick,
+  variant = "contained",
+  color = "primary",
+  sx = {},
+}) {
+  return (
+    <Button variant={variant} color={color} onClick={onClick} sx={sx}>
+      {text}
+    </Button>
+  );
+}
